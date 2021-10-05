6fa4a67f6d2cb8a9af41a57dad0ff4425679033b
Nin <77528189+Ninn08@users.noreply.github.com>
2021-10-05 16:52:15 +0530

    Delete commands/test directory

--- commands/test/sampleCmd.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "latency of the bot to discord",
-
-    execute: async(client, message, args) => {
-        return message.reply("Pong!")
-    }
-}
